f638a7d69f7f66de54137a77b331b451d184849e
MrOkun <74727162+MrOkun@users.noreply.github.com>
2021-09-01 22:45:04 +0300

    Добавьте файлы проекта.

---
+++ dither/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace dither
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Primordia_Image_Plane = new System.Windows.Forms.Panel();
+            this.Modified_Image = new System.Windows.Forms.PictureBox();
+            this.Primordial_Image = new System.Windows.Forms.PictureBox();
+            this.Modified_Image_Plane = new System.Windows.Forms.Panel();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.Load_Button = new System.Windows.Forms.Button();
+            this.Dither = new System.Windows.Forms.Button();
+            this.Steps_Bar = new System.Windows.Forms.TrackBar();
+            this.Method_Selector = new System.Windows.Forms.CheckBox();
+            this.Primordia_Image_Plane.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Modified_Image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Primordial_Image)).BeginInit();
+            this.Modified_Image_Plane.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Steps_Bar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Primordia_Image_Plane
+            // 
+            this.Primordia_Image_Plane.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(143)))));
+            this.Primordia_Image_Plane.Controls.Add(this.Primordial_Image);
+            this.Primordia_Image_Plane.Location = new System.Drawing.Point(12, 12);
+            this.Primordia_Image_Plane.Name = "Primordia_Image_Plane";
+            this.Primordia_Image_Plane.Size = new System.Drawing.Size(310, 310);
+            this.Primordia_Image_Plane.TabIndex = 0;
+            // 
+            // Modified_Image
+            // 
+            this.Modified_Image.Location = new System.Drawing.Point(3, 3);
+            this.Modified_Image.Name = "Modified_Image";
+            this.Modified_Image.Size = new System.Drawing.Size(304, 304);
+            this.Modified_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Modified_Image.TabIndex = 0;
+            this.Modified_Image.TabStop = false;
+            // 
+            // Primordial_Image
+            // 
+            this.Primordial_Image.Location = new System.Drawing.Point(3, 3);
+            this.Primordial_Image.Name = "Primordial_Image";
+            this.Primordial_Image.Size = new System.Drawing.Size(304, 304);
+            this.Primordial_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Primordial_Image.TabIndex = 0;
+            this.Primordial_Image.TabStop = false;
+            // 
+            // Modified_Image_Plane
+            // 
+            this.Modified_Image_Plane.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(143)))));
+            this.Modified_Image_Plane.Controls.Add(this.Modified_Image);
+            this.Modified_Image_Plane.Location = new System.Drawing.Point(328, 12);
+            this.Modified_Image_Plane.Name = "Modified_Image_Plane";
+            this.Modified_Image_Plane.Size = new System.Drawing.Size(310, 310);
+            this.Modified_Image_Plane.TabIndex = 1;
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.BackColor = System.Drawing.Color.White;
+            this.Save_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Save_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save_Button.Font = new System.Drawing.Font("Montserrat Thin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Save_Button.ForeColor = System.Drawing.Color.Black;
+            this.Save_Button.Location = new System.Drawing.Point(178, 393);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(141, 46);
+            this.Save_Button.TabIndex = 2;
+            this.Save_Button.Text = "Save";
+            this.Save_Button.UseVisualStyleBackColor = false;
+            this.Save_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // Load_Button
+            // 
+            this.Load_Button.BackColor = System.Drawing.Color.White;
+            this.Load_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Load_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Load_Button.Font = new System.Drawing.Font("Montserrat Thin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Load_Button.ForeColor = System.Drawing.Color.Black;
+            this.Load_Button.Location = new System.Drawing.Point(15, 393);
+            this.Load_Button.Name = "Load_Button";
+            this.Load_Button.Size = new System.Drawing.Size(141, 46);
+            this.Load_Button.TabIndex = 3;
+            this.Load_Button.Text = "Load";
+            this.Load_Button.UseVisualStyleBackColor = false;
+            this.Load_Button.Click += new System.EventHandler(this.Load_Button_Click);
+            // 
+            // Dither
+            // 
+            this.Dither.BackColor = System.Drawing.Color.White;
+            this.Dither.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Dither.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Dither.Font = new System.Drawing.Font("Montserrat Thin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Dither.ForeColor = System.Drawing.Color.Black;
+            this.Dither.Location = new System.Drawing.Point(497, 393);
+            this.Dither.Name = "Dither";
+            this.Dither.Size = new System.Drawing.Size(141, 46);
+            this.Dither.TabIndex = 4;
+            this.Dither.Text = "Dither";
+            this.Dither.UseVisualStyleBackColor = false;
+            this.Dither.Click += new System.EventHandler(this.Dither_Click);
+            // 
+            // Steps_Bar
+            // 
+            this.Steps_Bar.Location = new System.Drawing.Point(15, 328);
+            this.Steps_Bar.Maximum = 20;
+            this.Steps_Bar.Minimum = 1;
+            this.Steps_Bar.Name = "Steps_Bar";
+            this.Steps_Bar.Size = new System.Drawing.Size(304, 45);
+            this.Steps_Bar.TabIndex = 1;
+            this.Steps_Bar.Value = 1;
+            // 
+            // Method_Selector
+            // 
+            this.Method_Selector.AutoSize = true;
+            this.Method_Selector.Location = new System.Drawing.Point(555, 328);
+            this.Method_Selector.Name = "Method_Selector";
+            this.Method_Selector.Size = new System.Drawing.Size(68, 17);
+            this.Method_Selector.TabIndex = 5;
+            this.Method_Selector.Text = "B and W";
+            this.Method_Selector.UseVisualStyleBackColor = true;
+            this.Method_Selector.CheckStateChanged += new System.EventHandler(this.Method_Selector_CheckStateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(653, 451);
+            this.Controls.Add(this.Method_Selector);
+            this.Controls.Add(this.Steps_Bar);
+            this.Controls.Add(this.Dither);
+            this.Controls.Add(this.Load_Button);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.Modified_Image_Plane);
+            this.Controls.Add(this.Primordia_Image_Plane);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Primordia_Image_Plane.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Modified_Image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Primordial_Image)).EndInit();
+            this.Modified_Image_Plane.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Steps_Bar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Primordia_Image_Plane;
+        private System.Windows.Forms.PictureBox Modified_Image;
+        private System.Windows.Forms.PictureBox Primordial_Image;
+        private System.Windows.Forms.Panel Modified_Image_Plane;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Button Load_Button;
+        private System.Windows.Forms.Button Dither;
+        private System.Windows.Forms.TrackBar Steps_Bar;
+        private System.Windows.Forms.CheckBox Method_Selector;
+    }
+}
+
---
+++ dither/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dither
+{
+    public partial class Form1 : Form
+    {
+        private string _path = "0.png";
+        private bool _DoBW = true;
+
+        private Bitmap _primordialImage;
+        private Bitmap _modifiedImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Primordial_Image.Image = new Bitmap(_path, true);
+            Modified_Image.Image = new Bitmap(_path, true);
+        }
+
+
+
+        private Bitmap ImageRender(Bitmap Img)
+        {
+
+
+            return Img;
+        }
+
+        private void Dither_Click(object sender, EventArgs e)
+        {
+            _modifiedImage = new Bitmap(_path, true);
+
+            makeDithered(_modifiedImage, Steps_Bar.Value);
+            //_DoBW
+
+            if (!_DoBW)
+            {
+                int x, y;
+
+                for (x = 0; x < _modifiedImage.Width; x++)
+                {
+                    for (y = 0; y < _modifiedImage.Height; y++)
+                    {
+                        Color pixelColor1 = _modifiedImage.GetPixel(x, y);
+
+                        int grayscaleColor = (pixelColor1.R + pixelColor1.G + pixelColor1.B) / 3;
+
+                        Color newColor = Color.FromArgb(grayscaleColor, grayscaleColor, grayscaleColor);
+                        _modifiedImage.SetPixel(x, y, newColor);
+                    }
+                }
+            }
+
+            Modified_Image.Image = _modifiedImage;
+        }
+
+        private double closestStep(int max, int steps, int value)
+        {
+            return Math.Round((steps * value) /255 * Math.Floor(255 / (double)steps));
+        }
+
+        private void makeDithered(Bitmap img, int steps)
+        {
+            for (var y = 0; y < img.Height; y++)
+            {
+                for (var x = 0; x < img.Width; x++)
+                {
+                    var clr = img.GetPixel(x, y);
+                    var oldR = clr.R;
+                    var oldG = clr.G;
+                    var oldB = clr.B;
+                    var newR = closestStep(255, steps, oldR);
+                    var newG = closestStep(255, steps, oldG);
+                    var newB = closestStep(255, steps, oldB);
+
+                    var newClr = Color.FromArgb((int)newR, (int)newG, (int)newB);
+                    img.SetPixel(x, y, newClr);
+
+                    var errR = oldR - newR;
+                    var errG = oldG - newG;
+                    var errB = oldB - newB;
+
+                    distributeError(img, x, y, errR, errG, errB);
+                }
+            }
+        }
+
+        private void distributeError(Bitmap img, int x, int y, double errR, double errG, double errB)
+        {
+            addError(img, 7 / 16.0, x + 1, y, errR, errG, errB);
+            addError(img, 3 / 16.0, x - 1, y + 1, errR, errG, errB);
+            addError(img, 5 / 16.0, x, y + 1, errR, errG, errB);
+            addError(img, 1 / 16.0, x + 1, y + 1, errR, errG, errB);
+        }
+
+        private void addError(Bitmap img, double factor, int x, int y, double errR, double errG, double errB)
+        {
+            if (x < 0 || x >= img.Width || y < 0 || y >= img.Height) return;
+            var clr = img.GetPixel(x, y);
+            var r = clr.R;
+            var g = clr.G;
+            var b = clr.B;
+
+            int Red = (int)(r + errR * factor);
+            int Green = (int)(g + (errG * factor));
+            int Blue = (int)(b + errB * factor);
+
+            if (Red > 255)
+            {
+                Red = 255;
+            }
+            if (Green > 255)
+            {
+                Green = 255;
+            }
+            if (Blue > 255)
+            {
+                Blue = 255;
+            }
+
+            var newClr = Color.FromArgb(Red, Green, Blue);
+
+            img.SetPixel(x, y, newClr);
+        }
+
+
+        private void Load_Button_Click(object sender, EventArgs e)
+        {
+            Primordial_Image.Image = new Bitmap(_path, true);
+            Modified_Image.Image = new Bitmap(_path, true);
+        }
+
+        private void Save_Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Method_Selector_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (Method_Selector.Checked)
+            {
+                _DoBW = false;
+            }
+            else
+            {
+                _DoBW = true;
+            }
+        }
+    }
+}
